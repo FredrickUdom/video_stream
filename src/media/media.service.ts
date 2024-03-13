028552f3bcb41e9ee6e164fa35bb4ae8e991bd08
fredrickUdom <fredrickudom@gmail.com>
2024-03-14 00:09:44 +0100

    hot fix

--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -5,16 +5,10 @@ import { ensureDir, writeFile } from 'fs-extra';
 
 @Injectable()
 export class MediaService {
-  async saveMedia(
-    mediaFile: Express.Multer.File,
-    folder = 'default',
-  ): Promise<IMediaResponse> {
+  async saveMedia(mediaFile: Express.Multer.File, folder = 'default',): Promise<IMediaResponse> {
     const uploadFolder = `${path}/uploads/${folder}`;
     await ensureDir(uploadFolder);
-    await writeFile(
-      `${uploadFolder}/${mediaFile.originalname}`,
-      mediaFile.buffer,
-    );
+    await writeFile( `${uploadFolder}/${mediaFile.originalname}`, mediaFile.buffer,);
 
     return {
       url: `/uploads/${folder}/${mediaFile.originalname}`,
